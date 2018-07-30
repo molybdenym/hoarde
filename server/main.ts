917296343159c7681193c37afb41827ce592c0b0
Paul Laskowski <paul@warmbowski.org>
2018-07-30 00:36:11 -0700

    added apollo server setup

--- server/main.ts
+++ server/main.ts
@@ -1,34 +1,38 @@
-import { WebApp } from 'meteor/webapp';
+// import { WebApp } from 'meteor/webapp';
 import * as express from 'express';
-import * as bodyParser from 'body-parser';
+// import * as bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
-import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
-import * as proxyMiddleware from 'http-proxy-middleware';
+// import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import { ApolloServer, gql } from 'apollo-server-express';
+// import * as proxyMiddleware from 'http-proxy-middleware';
 
 // import schema from '../imports/api/index';
 // import Mocks from '../imports/api/mocks';
-import typeDefs from '../imports/api/schema';
-import resolvers from '../imports/api/resolvers';
+// import typeDefs from '../imports/api/schema';
+// import resolvers from '../imports/api/resolvers';
 
 
-const graphQLServer = express();
+// const graphQLServer = express();
 const GRAPHQL_PORT = 4000;
 
-const executableSchema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
-});
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!'
+  },
+};
 
-// addMockFunctionsToSchema({
-//   schema: executableSchema,
-//   mocks: Mocks,
-//   preserveResolvers: true,
-// });
+const server = new ApolloServer({ typeDefs, resolvers });
 
-graphQLServer.use(bodyParser.urlencoded({ extended: true }));
-graphQLServer.use(bodyParser.json());
-graphQLServer.use('/graphql', graphqlExpress({ schema: executableSchema }));
-graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-graphQLServer.listen(GRAPHQL_PORT, () => console.log(`GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`));
+const app = express();
+server.applyMiddleware({ app });
 
-// WebApp.rawConnectHandlers.use(proxyMiddleware({ target: `http://localhost:${GRAPHQL_PORT}/graphql` }));
+app.listen({ port: GRAPHQL_PORT }, () =>
+  console.log(`🚀 Server ready at http://localhost:${GRAPHQL_PORT}${server.graphqlPath}`),
+);
